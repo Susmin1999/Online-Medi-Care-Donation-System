e0f5ca5585c95528e588adc80ec2d8a64abea8ad
Susmin1999 <78251581+Susmin1999@users.noreply.github.com>
2022-07-21 16:10:27 +0530

    Add files via upload

---
+++ medicalshop/include/sidebar.php
@@ -0,0 +1,286 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<!-- <li>
+								<a href="requestapproval.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Request Approval</span>
+										</div>
+									</div>
+								</a>
+							</li> -->
+
+
+							
+
+                            
+                            <!-- <li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title">Manager </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="addmanager.php">
+											<span class="title"> Add Manager</span>
+										</a>
+									</li>	
+									<li>
+										<a href="viewmanager.php">
+											<span class="title"> View Manager</span>
+										</a>
+									</li>	
+								</ul>
+								</li>	 -->
+
+								<li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+
+										
+											<div class="item-inner">
+												<span class="title">Medicines </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="mshopaddmedicine.php">
+												<span class="title"> Add Medicines</span>
+											</a>
+										</li>	
+										<li>
+											<a href="viewmedicine.php">
+												<span class="title"> Update Medicines</span>
+											</a>
+										</li>	
+									</ul>
+									</li>	
+
+<!-- 										
+								<li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+
+										
+											<div class="item-inner">
+												<span class="title">Medicine Category </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="addmedcategory.php">
+												<span class="title"> Add Medicine Category</span>
+											</a>
+										</li>	
+										<li>
+											<a href="viewmedcategory.php">
+												<span class="title"> Actions/View Category</span>
+											</a>
+										</li>	
+									</ul>
+									</li>	 -->
+
+
+
+									<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+
+									<div class="item-inner">
+												<span class="title">View Approval Status </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="viewmshopapprstatus.php">
+												<span class="title"> View Status</span>
+											</a>
+										</li>	
+										
+									</ul>
+									</li>	
+
+
+									<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+
+									<div class="item-inner">
+												<span class="title"> Donors </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="approveddonreq.php">
+												<span class="title"> View Donors </span>
+											</a>
+										</li>	
+										
+									</ul>
+									</li>	
+
+
+
+									<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+
+									<div class="item-inner">
+												<span class="title"> Registered Patients </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="viewpatient.php">
+												<span class="title"> View Patients</span>
+											</a>
+										</li>									
+									</ul>
+									</li>	
+
+
+
+									<!-- <li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+
+									<div class="item-inner">
+												<span class="title"> Donated Medicines </span><i class="icon-arrow"></i>
+											</div>
+										</div>
+									</a>
+									<ul class="sub-menu">
+										
+										<li>
+											<a href="viewdonatedmedicine.php">
+												<span class="title"> View Donated Medicines</span>
+											</a>
+										</li>	
+										
+									</ul>
+									</li>	 -->
+
+									<li>
+								<a href="viewenquiry.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Approved Medicine Request </span>
+										</div>
+									</div>
+								</a>
+							</li>
+	
+					
+
+								<li>
+								<a href="viewpayment.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Payment Status </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="viewfeedback.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Patient Feedback </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<!-- <li>
+								<a href="viewmedicines.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> View Medicines</span>
+										</div>
+									</div>
+								</a>
+							</li> -->
+
+
+
+                            
+							
+
+				<li>						
+					</nav>
+					</div>
+			</div>
